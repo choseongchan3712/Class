1ed6502c48487fcad1bb6307e2dd48699fbd1558
choseongchan3712 <choseongchan3712@naver.com>
2024-10-31 03:21:09 +0900

    타입스크립트 해보기

---
+++ react-typescript/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GlobalStyled } from './GlobalStyled';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <GlobalStyled />
+  </React.StrictMode>
+);
+
